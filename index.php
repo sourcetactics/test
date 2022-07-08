44d32dc1ffb970f2343f8d1329d82eee40fdde67
sourcetactics <aforchewski@gmail.com>
2022-07-08 17:08:51 +0200

    final

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+define("MAX_INT",9223372036854775807);
 
 class Calculation{
     private $x;
@@ -8,11 +9,23 @@ class Calculation{
     private function l_b_calc(){
         $x = $this->x - $this->radius;
         $y = $this->y - $this->radius;
+        if($x > MAX_INT || $x < (MAX_INT * -1)){
+          $x = number_format($x,0,'.','');
+        }
+        if($y > MAX_INT || $y < (MAX_INT * -1)){
+          $y = number_format($y,0,'.','');
+        }
         return ["x" => $x, "y" => $y];
     }
     private function r_t_calc(){
       $x = $this->x + $this->radius;
       $y = $this->y + $this->radius;
+      if($x > MAX_INT || $x < (MAX_INT * -1)){
+        $x = number_format($x,0,'.','');
+      }
+      if($y > MAX_INT || $y < (MAX_INT * -1)){
+        $y = number_format($y,0,'.','');
+      }
       return ["x" => $x, "y" => $y];
     }
     private function result_opt(){
@@ -44,7 +57,7 @@ class Min_area_rectangle{
     $data_results = [];
 
     for($i = 0; $i < count($data); $i++){
-       echo 1;
+      
       for($a = 0; $a < count($data[$i]); $a++){
 
           $x_left_static = $data[$i][$a]["left_bottom"]["x"];
@@ -65,16 +78,16 @@ class Min_area_rectangle{
               $y_left = $data[$i][$e]["left_bottom"]["y"];
               $y_right = $data[$i][$e]["right_top"]["y"];
 
-              if($x_left_static > $x_left){
+              if((int) $x_left_static > (int) $x_left){
                   $x_min_flag = false;
               }
-              if($y_left_static > $y_left){
+              if((int) $y_left_static > (int) $y_left){
                   $y_min_flag = false;
               }
-              if($x_right_static < $x_right){
+              if((int) $x_right_static < (int) $x_right){
                   $x_max_flag = false;
               }
-              if($y_right_static < $y_right){
+              if((int) $y_right_static < (int) $y_right){
                   $y_max_flag = false;
               }
               
@@ -115,43 +128,61 @@ $data = json_decode($json);
 
 
 
+
+
+
 $ch = curl_init('http://contest.elecard.ru/api');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-//curl_setopt($ch, CURLOPT_POSTFIELDS, $result);
 
+$html = curl_exec($ch);
 
-$html = curl_exec($ch); // ресурс curl
-
-curl_close($ch);
  
 
 
 $tests = json_decode($html,true);
-//var_dump($tests);
 $calc = new Calculation();
 $area_class = new Min_area_rectangle();
 $results_cords = $calc->loop($tests);
-var_dump($area_class->calc_area($results_cords));
+$results_areas = $area_class->calc_area($results_cords);
+$results_areas = json_encode($results_areas);
 
+
+$result_query = '{
+  "key":"LT+DW4pE3iWU+YMKTD0F2GogvwD9k/QhpCVbDZUYsODVhiffVDQcl/Vn+MqQ2LhoxGDSkTBZasj2jlPgKRUclQ==",
+  "method":"CheckResults",
+  "params": '.$results_areas.'
+  
+}';
+
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $result_query);
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+$html = curl_exec($ch);
+
+echo $html;
+
+curl_close($ch);
 
 /*
+foreach ($tests["result"] as $value) {
+  var_dump($value);
+  echo "l_b x:" .$value["left_bottom"]["x"]."<br>";
+  echo "l_b y:" .$value["left_bottom"]["y"] ."<br>";
+  echo "r_t x:" .$value["right_top"]["x"]."<br>";
+  echo "r_t y:" .$value["right_top"]["y"] ."<hr>";
 
-$result = '{
-    "key":"LT+DW4pE3iWU+YMKTD0F2GogvwD9k/QhpCVbDZUYsODVhiffVDQcl/Vn+MqQ2LhoxGDSkTBZasj2jlPgKRUclQ==",
-    "method":"CheckResults",
-    "params": [
-      {
-        "left_bottom":{"x":0,"y":0},
-        "right_top":{"x":0,"y":0}
-      }
-    ]
-    
-  }';
-$result = json_decode($result,true);
+}
+foreach ($results_areas as $value) {
+   echo "l_b x:" .$value["left_bottom"]["x"]."<br>";
+   echo "l_b y:" .$value["left_bottom"]["y"] ."<br>";
+   echo "r_t x:" .$value["right_top"]["x"]."<br>";
+   echo "r_t y:" .$value["right_top"]["y"] ."<hr>";
+
+}
 */
 
 ?>
